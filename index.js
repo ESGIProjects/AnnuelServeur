334bffb8d0d1b1e2cb1f30454b7116426dbf77ff
Kevin Le <Kevin Le>
2017-04-14 09:36:40 +0200

    Test Firebase

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/alert', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-    res.send(process.env.privateKey);
+    res.send(key.replace("\\n", "\n"));
 });
 
 app.listen(port, function() {
