942f2f31b25503c80ff0f044acf0b5998aaca79c
Jason Pierna <siilver777@me.com>
2017-04-23 17:03:51 +0200

    Adds Moongose, retrieve token for /alert route

--- index.js
+++ index.js
@@ -1,77 +1,109 @@
+// Dépendances
 var express = require("express");
 var admin = require("firebase-admin");
-var pg = require("pg");
 
+// Nécessaire pour laisser Heroku contrôler le port
 var port = process.env.PORT || 8080;
-
 var app = express();
 
+// Middleware pour parser les requêtes POST
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true})) ;
 
-var registrationToken = "cyddXI_muVg:APA91bHaaVq6RgytkpGLdrUC-Px73uvU-oqyRdFwoq621_K154NkaRrHKGEkY3wr6AbP4OTBYjVeR_9afvJ7h5Lhyt369-K7vk25Rpa1JfbqVyYRSUqbjeg6KBJ51IpBWsNAfVhUA7Fz";
+// Clé Firebase
 var key = process.env.privateKey;
 
+// Notification payload
 var payload = {
-  notification: {
-    title: "Motion detected !",
-    body: "Somebody is in your house !"
-  }
+    notification: {
+        title: "Motion detected !",
+        body: "Somebody is in your house !"
+    }
 };
 
+// Mise en place Firebase
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.projectId,
-    clientEmail: process.env.clientEmail,
-    privateKey:  key.replace(/\\n/g, '\n')
-  }),
-  databaseURL: "https://jarvis-773da.firebaseio.com/"
+    credential: admin.credential.cert({
+        projectId: process.env.projectId,
+        clientEmail: process.env.clientEmail,
+        privateKey:  key.replace(/\\n/g, '\n')
+    }),
+    databaseURL: "https://jarvis-773da.firebaseio.com/"
 });
 
+// Mise en place mongoose
+var mongoose = require('mongoose');
+var DeviceToken = mongoose.model('DeviceToken', new mongoose.Schema({
+    token: String
+}));
+
+process.env.MONGODB_URI = 'mongodb://heroku_30zt34kx:d70kbje1r1apa2reje1tjrtvtt@ds115131.mlab.com:15131/heroku_30zt34kx';
+mongoose.connect(process.env.MONGODB_URI, function(error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+// Route envoyant une simple notification d'alerte
 app.post('/alert', function(req, res) {
-    admin.messaging().sendToDevice(registrationToken,payload)
-    .then(function(response){
-      console.log("Success ! ", response);
-      res.status(200).send("Success");
-    })
-    .catch(function(error){
-      console.log("Error ! ", error);
-    })
-});
 
-app.post('/registerToken', function(req,res) {
-    var deviceToken = req.body.deviceToken;
-    //var pgurl = 'postgres://ppticfpidgeahq:fdf2b36f67cd135f548fba7db4790e998b0f49b99fa19589b838ade05a7a90e5@ec2-79-125-125-97.eu-west-1.compute.amazonaws.com:5432/d4badkjb2buqr';
-
-    console.log('Device token is : ' + deviceToken);
-
-    /*pg.defaults.ssl = true;
-    pg.connect(pgurl, function(err, client) {
-    if (err) throw err;
-    console.log('Connecté à la BDD');
-
-    client.query('INSERT INTO devicetokens (token_string) VALUES($1);', [deviceToken], function(err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
-    }); */
-});
-
-app.get('/displayToken', function(req, res) {
-    /*var pgurl = 'postgres://ppticfpidgeahq:fdf2b36f67cd135f548fba7db4790e998b0f49b99fa19589b838ade05a7a90e5@ec2-79-125-125-97.eu-west-1.compute.amazonaws.com:5432/d4badkjb2buqr';
-
-    pg.defaults.ssl = true;
-    pg.connect(pgurl, function(err, client) {
-        if (err) throw err;
-        console.log('Connected to pg! Getting schemas...');
-
-        client
-            .query('SELECT table_schema, table_name FROM information_schema.tables;')
-            .on('row', function(row) {
-                console.log(JSON.stringify(row));
+    // Récupérer le dernier token
+    DeviceToken
+    .findOne()
+    .sort({'_id': -1})
+    .exec(function(error, deviceToken) {
+        if (error) {
+            console.error(error);
+            res.status(500);
+        } else {
+            // Envoyer la notification
+            admin
+            .messaging()
+            .sendToDevice(deviceToken.token, payload)
+            .then(function(res) {
+                console.log("Success ! ", res);
+                res.status(200).send("Success");
+            })
+            .catch(function(error){
+                console.error("Error ! ", error);
             });
-    }); */
+        }
+    });
+});
+
+// Route permettant d'enregistrer un token dans la base de données
+app.post('/registerToken', function(req,res) {
+
+    // Vérification du paramètre
+    if (req.body.deviceToken === undefined) {
+        res.status(400);
+    }
+
+    // Création de l'objet à stocker
+    var token = new DeviceToken({
+        "token": req.body.deviceToken
+    });
+
+    // Stockage de l'objet
+    token.save(function(error) {
+        if (error) {
+            console.error(error);
+            res.status(500);
+        } else {
+            res.status(201).json(token);
+        }
+    });
+});
+
+// Debug affichage bdd token
+app.get('/debug/displayAllTokens', function(req, res) {
+    DeviceToken.find(function(error, tokens) {
+        if (error) {
+            console.error(error);
+            res.status(500);
+        }
+        res.status(200).json(tokens);
+    });
 });
 
 app.post('/updatePassword', function(req,res){
@@ -82,6 +114,7 @@ app.get('/', function(req, res) {
     res.send(port);
 });
 
+// Lancement de l'application
 app.listen(port, function() {
     console.log('Running on port ' + port);
 });
